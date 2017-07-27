205a91ab4dcc33db869d5ec7a394b3fab57437de
Daniel Jilg <winsmith@winsmith.de>
2017-07-27 20:40:08 +0200

    Add Docs for Views

--- ablator/api/views.py
+++ ablator/api/views.py
@@ -8,6 +8,18 @@ from core.models import ClientUser, FunctionalityGroup
 
 class CanIUseView(APIView):
     def get(self, request, client_user_string, functionality_group_id):
+        """
+        Is the specified user allowed to use the feature?
+
+        Returns `{ "enabled": true }` if the specified ClientUser is allowed to use the
+        functionality group, ``{ "enabled": false }`` if the user is disallowed, or a 404 error if
+        functionality group does not exist.
+
+        Include a string that uniquely identifies the user as the client user string.
+
+        See also the `which` endpoint to see if a user has a specific functionality group within a
+        functionality
+        """
         functionality_group = get_object_or_404(FunctionalityGroup, id=functionality_group_id)
         client_user = ClientUser.user_from_object(client_user_string)
         return Response({'enabled': can_i_use(client_user, functionality_group)})
@@ -15,6 +27,16 @@ class CanIUseView(APIView):
 
 class WhichView(APIView):
     def get(self, request, client_user_string, functionality_group_id):
+        """
+        Which Functionality of the given FunctionalityGroup is enabled for the user, if any?
+
+        Returns `{ "which": "<app.group.functionality>" }` that corresponds to the ClientUser's
+        enabled functionality, or `{ "which": none }` if the user does not have any Functionality in
+        the given FuncationlityGroup.
+
+        If the FunctionalityGroup does not exist, this endpoint returns a 404 error.
+
+        """
         functionality_group = get_object_or_404(FunctionalityGroup, id=functionality_group_id)
         client_user = ClientUser.user_from_object(client_user_string)
         functionality = which(client_user, functionality_group)
--- ablator/core/functionality.py
+++ ablator/core/functionality.py
@@ -1,9 +1,18 @@
 from typing import Optional
 
-from core.models import FunctionalityGroup, Functionality, ClientUser, Availability
+from .models import FunctionalityGroup, Functionality, ClientUser, Availability
 
 
 def can_i_use(client_user: ClientUser, functionality_group: FunctionalityGroup) -> bool:
+    """
+    Is the specified user allowed to use the feature?
+
+    Returns `True` if the specified ClientUser is allowed to use the functionality group,
+    `False` if the user is disallowed, or the functionality group does not exist.
+
+    Use ClientUser.user_from_object to get or create a ClientUser instance from any hashable
+    object (usually a string).
+    """
     return which(client_user, functionality_group) is not None
 
 
@@ -11,6 +20,15 @@ def which(
         client_user: ClientUser,
         functionality_group: FunctionalityGroup
 ) -> Optional[Functionality]:
+    """
+    Which Functionality of the given FunctionalityGroup is enabled for the user, if any?
+
+    Returns a Functionality object that corresponds to the ClientUser's enabled functionality,
+    or `None` if the user does not have any Functionality in the given FunctionalityGroup.
+
+    Use ClientUser.user_from_object to get or create a ClientUser instance from any hashable
+    object (usually a string).
+    """
     try:
         return Availability.objects.get(
             functionality__in=functionality_group.functionality_set.all(),
