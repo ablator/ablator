d500db9ce2d944c599d44dd0bbba0739e2782adf
Daniel Jilg <winsmith@winsmith.de>
2017-08-04 09:18:03 +0200

    Add Login and Logout

--- ablator/web_admin/urls.py
+++ ablator/web_admin/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import url
 from rest_framework.urlpatterns import format_suffix_patterns
 
-from web_admin.views import HomePageView
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    url('$', HomePageView.as_view(), name='home')
+    url(r'^accounts/login/$', auth_views.LoginView.as_view(), name='login'),
+    url(r'^accounts/logout/$', auth_views.logout, name='logout'),
+    url('$', views.HomePageView.as_view(), name='home'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- ablator/web_admin/views.py
+++ ablator/web_admin/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render
+from django.contrib.admin.views.decorators import staff_member_required
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 from django.views.generic.base import TemplateView
 
 from core.models import App
 
 
+@method_decorator(login_required, name='dispatch')
 class HomePageView(TemplateView):
     template_name = 'index.html'
 
