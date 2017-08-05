2a3feef49cea889849a7dd2626d33b074d5cfc51
Daniel Jilg <winsmith@winsmith.de>
2017-08-05 17:23:56 +0200

    Create base template
    fix #24

--- ablator/web_admin/urls.py
+++ ablator/web_admin/urls.py
@@ -6,7 +6,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^accounts/login/$', auth_views.LoginView.as_view(), name='login'),
-    url(r'^accounts/logout/$', auth_views.logout, name='logout'),
+    url(r'^accounts/logout/$', auth_views.LogoutView.as_view(), name='logout'),
     url('$', views.HomePageView.as_view(), name='home'),
 ]
 
