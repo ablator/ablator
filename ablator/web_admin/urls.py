161c5f4893feaac085f7f0461c504064db1c435c
Daniel Jilg <winsmith@winsmith.de>
2017-08-05 20:50:22 +0200

    Add functionality view
    #16

--- ablator/web_admin/urls.py
+++ ablator/web_admin/urls.py
@@ -7,6 +7,8 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     url(r'^accounts/login/$', auth_views.LoginView.as_view(), name='login'),
     url(r'^accounts/logout/$', auth_views.LogoutView.as_view(), name='logout'),
+    url(r'^app/(?P<app_id>[^/]+)/$', views.AppView.as_view(), name='app'),
+    url(r'^functionality/(?P<functionality_id>[^/]+)/$', views.FunctionalityView.as_view(), name='functionality'),
     url('$', views.HomePageView.as_view(), name='home'),
 ]
 
