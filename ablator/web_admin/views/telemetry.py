ae1c50b9aff0db0b6ce1effe89433902767de686
Daniel Jilg <winsmith@winsmith.de>
2020-02-12 14:57:37 +0100

    Show users for last day

--- ablator/web_admin/views/telemetry.py
+++ ablator/web_admin/views/telemetry.py
@@ -21,17 +21,21 @@ class SignalListView(ListView):
 
         one_month_ago = datetime.date.today() - datetime.timedelta(days=30)
         one_week_ago = datetime.date.today() - datetime.timedelta(days=7)
+        one_day_ago = datetime.date.today() - datetime.timedelta(days=1)
 
         app = App.objects.filter(organization=self.request.user.ablatoruser.organization).get(id=self.kwargs['app_id'])
         app_signals = Signal.objects.filter(type__app=app)
 
         last_month_signals = app_signals.filter(received_at__gte=one_month_ago)
         last_week_signals = app_signals.filter(received_at__gte=one_week_ago)
+        last_day_signals = app_signals.filter(received_at__gte=one_day_ago)
 
         last_month_users = ClientUser.objects.filter(signal__in=last_month_signals).distinct()
-        last_week_users = ClientUser.objects.filter(signal__in=last_week_signals).distinct()
+        last_week_users = last_month_users.filter(signal__in=last_week_signals).distinct()
+        last_day_users = last_month_users.filter(signal__in=last_day_signals).distinct()
 
         context['active_users_last_month'] = last_month_users.count()
         context['active_users_last_week'] = last_week_users.count()
+        context['active_users_last_day'] = last_day_users.count()
 
         return context
