fa4d0e323ba9e142ca00731ba93ee74067da95d4
Daniel Jilg <winsmith@winsmith.de>
2020-03-10 16:12:08 +0100

    Improve Telemetry View Performance

--- ablator/web_admin/views/telemetry.py
+++ ablator/web_admin/views/telemetry.py
@@ -25,6 +25,7 @@ def accumulated_dictionary_data_source(signals: list, signal_parameter_key: str)
     data_source = SimpleDataSource(data=data)
     return data_source
 
+
 def create_source_type_chart(signals):
     source_version_count = {
         "isTestFlight": 0,
@@ -55,7 +56,7 @@ class SignalListView(ListView):
 
     def get_queryset(self):
         app = App.objects.filter(organization=self.request.user.ablatoruser.organization).get(id=self.kwargs['app_id'])
-        return Signal.objects.filter(type__app=app).order_by("-received_at")[:200]
+        return Signal.objects.filter(type__app=app).order_by("-received_at")[:10]
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -64,42 +65,43 @@ class SignalListView(ListView):
         one_week_ago = datetime.date.today() - datetime.timedelta(days=7)
         one_day_ago = datetime.date.today() - datetime.timedelta(days=1)
 
-        app = App.objects.filter(organization=self.request.user.ablatoruser.organization).get(id=self.kwargs['app_id'])
+        app = App.objects.get(id=self.kwargs['app_id'], organization_id=self.request.user.ablatoruser.organization_id)
         app_signals = Signal.objects.filter(type__app=app)
 
         last_month_signals = app_signals.filter(received_at__gte=one_month_ago)
         last_week_signals = app_signals.filter(received_at__gte=one_week_ago)
         last_day_signals = app_signals.filter(received_at__gte=one_day_ago)
 
-        last_month_users = ClientUser.objects.filter(signal__in=last_month_signals).distinct()
-        last_week_users = last_month_users.filter(signal__in=last_week_signals).distinct()
-        last_day_users = last_month_users.filter(signal__in=last_day_signals).distinct()
+        distinct_last_month_signals = self.distinctivize(last_month_signals)
 
-        context['active_users_last_month'] = last_month_users.count()
-        context['active_users_last_week'] = last_week_users.count()
-        context['active_users_last_day'] = last_day_users.count()
+        context['active_users_last_month'] = len(distinct_last_month_signals)
+        context['active_users_last_week'] = len(self.distinctivize(last_week_signals))
+        context['active_users_last_day'] = len(self.distinctivize(last_day_signals))
 
         # Charts
         dim = 150
+        context['charts'] = [
+            {
+                "App Version": DonutChart(accumulated_dictionary_data_source(distinct_last_month_signals, "appVersion"), height=dim, width=dim),
+                "System Version": DonutChart(accumulated_dictionary_data_source(distinct_last_month_signals, "systemVersion"), height=dim, width=dim),
+                "Build Number": DonutChart(accumulated_dictionary_data_source(distinct_last_month_signals, "buildNumber"), height=dim, width=dim),
+                "Source Type": DonutChart(create_source_type_chart(distinct_last_month_signals), height=dim, width=dim),
+            },
+            {
+                "Libido Description Type": DonutChart(accumulated_dictionary_data_source(distinct_last_month_signals, "libidoDescriptionType"), height=dim, width=dim),
+                "Should Send Notifications": DonutChart(accumulated_dictionary_data_source(distinct_last_month_signals, "shouldSendExperienceSamplingNotifications"), height=dim, width=dim),
+                "Should Use HealthKit": DonutChart(accumulated_dictionary_data_source(distinct_last_month_signals, "shouldUseHealthKit"), height=dim, width=dim),
+
+            }
+        ]
+        return context
+
+    def distinctivize(self, signals):
         distinct_user_signals = []
         known_users = []
-
-        for signal in last_month_signals:
-            if signal.user in known_users:
+        for signal in signals:
+            if signal.user_id in known_users:
                 continue
-            known_users.append(signal.user)
+            known_users.append(signal.user_id)
             distinct_user_signals.append(signal)
-
-        context['charts'] = [{
-            "App Version": DonutChart(accumulated_dictionary_data_source(distinct_user_signals, "appVersion"), height=dim, width=dim),
-            "System Version": DonutChart(accumulated_dictionary_data_source(distinct_user_signals, "systemVersion"), height=dim, width=dim),
-            "Build Number": DonutChart(accumulated_dictionary_data_source(distinct_user_signals, "buildNumber"), height=dim, width=dim),
-            "Source Type": DonutChart(create_source_type_chart(distinct_user_signals), height=dim, width=dim),
-        },
-        {
-            "Libido Description Type": DonutChart(accumulated_dictionary_data_source(distinct_user_signals, "libidoDescriptionType"), height=dim, width=dim),
-            "Should Send Notifications": DonutChart(accumulated_dictionary_data_source(distinct_user_signals, "shouldSendExperienceSamplingNotifications"), height=dim, width=dim),
-            "Should Use HealthKit": DonutChart(accumulated_dictionary_data_source(distinct_user_signals, "shouldUseHealthKit"), height=dim, width=dim),
-
-        }]
-        return context
+        return distinct_user_signals
